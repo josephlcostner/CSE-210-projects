a20d4f538e2431aec23a90129a6b628edb8c6d31
josephlcostner <jcostner@byupathway.edu>
2026-01-09 13:55:36 -0700

    Exercise 2 program complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        {
+            Console.Write ("Out of 100, what is your grade? ");
+            string grade = Console.ReadLine();
+            int number = int.Parse(grade);
+
+            if (number <= 100)
+            {
+                Console.WriteLine("A");
+            }
+            else if (number <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            else if (number <= 79)
+            {
+                Console.WriteLine("C");
+            }
+            else if (number <= 69)
+            {
+                Console.WriteLine("D");
+            }
+            else if (number <= 59)
+            {
+                Console.WriteLine("F");
+            }
+            else
+            {
+                Console.WriteLine("This is not a valid number.");
+            }
+        }
     }
 }
