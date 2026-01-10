1169dbf2ddb64505636aa74a6e33a45f3fd10718
josephlcostner <jcostner@byupathway.edu>
2026-01-09 19:24:36 -0700

    Project 3 attempt (not my best work)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine (i);
+            }
+
+            Console.Write("Guess the magic number: ");
+            string guess = Console.ReadLine();
+            int Cardinal = int.Parse(guess);
+
+
+
+            if (number == Cardinal)
+            {
+                Console.WriteLine("That is correct!");
+            }
+
+            else if (number != Cardinal)
+            {
+                Console.WriteLine("Wrong number. Guess again: ");
+            }
+        }
+
     }
 }
