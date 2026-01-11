fbcdcfc687fd78e9708288cb5fbee2d76bf5371d
josephlcostner <jcostner@byupathway.edu>
2026-01-10 19:07:13 -0700

    Excersise 5. Here we go.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
@@ -6,4 +7,30 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static void PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+    }
+
+    static void SquaredNumber()
+    {
+        return PromptUserNumber * PromptUserNumber;
+    }
+
+    static void DisplayPersonalMessage()
+    {
+        Console.WriteLine($"Hello {PromptUserName}, the square of your favorite number is {SquaredNumber}");
+        return DisplayPersonalMessage;
+    }
+        
 }
