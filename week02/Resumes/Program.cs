392677c7954f219bcd1b1df682fe19931abee24c
josephlcostner <jcostner@byupathway.edu>
2026-01-13 12:23:47 -0700

    Changes made to W02 lesson 1

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,13 +9,16 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
 
-    public class Job
-    {
-      Job job1 = new Job();
-    public string _job1 = "";
+    
+   Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+    Job job2 = new Job();
+        job2._jobTitle = "Programmer";
+        job2._company = "Apple";
+        Console.WriteLine($"{job1._company}");
+        Console.WriteLine($"{job2._company}");
     }
- 
-    }
-
+    
     
 }
