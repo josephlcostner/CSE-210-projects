4d1e60ca204a71aeed30513e052c9c7cb8072966
josephlcostner <jcostner@byupathway.edu>
2026-01-13 12:48:30 -0700

    Current Progress with projects

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -14,15 +14,15 @@
     {
         public string _company = "";
         public string _jobTitle = "";
-        public string _startYear = "int";
-        public string _endYear = "int";
+        public string _startYear = "";
+        public string _endYear = "";
     
         public Job()
         {
         }
         public void Display()
         {
-            
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
         }
     
     }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -13,10 +13,15 @@ class Program
    Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+        job1.Display;
     Job job2 = new Job();
         job2._jobTitle = "Programmer";
         job2._company = "Apple";
-        Console.WriteLine($"{job1._company}");
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+        Console.WriteLine(job1.Display());
         Console.WriteLine($"{job2._company}");
     }
     
