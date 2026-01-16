ecbb3ecf03a2d78f027f7eebd973cd364be032e5
josephlcostner <jcostner@byupathway.edu>
2026-01-15 20:53:28 -0700

    Final Update

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -7,31 +7,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        PromptUserName();
+        int number = PromptFavoriteNumber();
+        SquaredNumber(number);
+        
     }
-    static void DisplayWelcome(string DisplayWelcome)
+
+    static void DisplayWelcome()
     {
         Console.WriteLine("Welcome to the Program!");
     }
 
-    static void PromptUserName(string PromptUserName)
+    static void PromptUserName()
     {
         Console.WriteLine("What is your name? ");
+        Console.ReadLine();
     }
 
-    static void PromptFavoriteNumber(int number)
+    public static int PromptFavoriteNumber()
     {
         Console.Write("What is your favorite number? ");
+        int FavoriteNumber = int.Parse(Console.ReadLine());
+        return FavoriteNumber;
     }
 
-    static void SquaredNumber(int number, int total)
-    {
+    static void SquaredNumber(int number)
+    {   int total;
         total = number * number;
-        
+        Console.WriteLine(total);
     }
 
-    static void DisplayPersonalMessage(string PromptUserName, string DisplayWelcome, int PromptFavoriteNumber, int SquaredNumber)
-    {
-        Console.WriteLine($"Hello {PromptUserName}, {DisplayWelcome}, the square of {PromptFavoriteNumber} is {SquaredNumber}");
-    }
+    
     
 }
