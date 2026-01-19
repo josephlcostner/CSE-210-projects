0a130e0fbf71ecfda08415cc2ea8b6810213d4e1
josephlcostner <jcostner@byupathway.edu>
2026-01-19 12:18:38 -0700

    Upload 1 for Module 1

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,24 @@
+public class Fraction
+{
+   private int _topNumber;
+   private int _bottomNumber; 
+   public class Function;
+   private class Numerator;
+   private class Denominator;
+   
+    public Fraction(int topNumber, int bottomNumber)
+        {
+            _topNumber = topNumber;
+            _bottomNumber = bottomNumber;
+        }
+    public int topNumber
+    {
+        get {return _topNumber; }
+    }
+
+    public int bottomNumber
+    {
+        get {return _bottomNumber; }
+    }
+   
+}
